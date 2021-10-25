6ded58701c4c1cb23d422b9168000eadec833e95
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2021-10-25 19:17:07 -0400

    started rendering parts of the header, will need to space and style, getting the hang of it, need to create a branch

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,7 @@
+import './Header.scss'
 import logo from '../../assets/images/Logo/BrainFlix-logo.svg'
+import defaultPic from '../../assets/images/Images/Mohan-muruge.jpg'
+import buttonIcon from '../../assets/images/Icons/upload.svg'
 
 export default function Header () {
   return (
@@ -8,7 +11,16 @@ export default function Header () {
         <img src={logo} 
         alt='this is the logo for the Brainflix website' />
         </a>
+          <div className='header__search__container'>
+          <form>
+            <input className="header__search" type='text' id='search' placeholder='Search'></input>
+          </form>
+          </div>
+          <img className = 'header__img' src={defaultPic} alt='small icon of default a person' />
       </div>
+      <button className='header__button'><img className='header__button__icon' 
+      src={buttonIcon} alt='upload icon'
+      />UPLOAD</button>
     </header>
   )
 }
