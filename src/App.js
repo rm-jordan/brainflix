4bc96cdf44ab106e7704c849fdc71d4fdfd47e5f
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2021-10-26 18:18:19 -0400

    got the image to render for the bmx lads, remember every piece of information comes from the json file passed down as props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './styles/app.scss'
 import Header from './components/Header/Header'
 import HeroVideo from './components/HeroVideo/HeroVideo'
-
+import allVideos from './data/videos.json'
 
 
 function App() {
   return (
     <>
     <Header />
-    <HeroVideo />
+    <HeroVideo allVideos={allVideos}/>
+
     </>
   );
 }
--- src/components/HeroVideo/HeroVideo.js
+++ src/components/HeroVideo/HeroVideo.js
@@ -1,8 +1,9 @@
 import './HeroVideo.scss'
-import fullScreen from '../../assets/images/Icons/fullscren.svg'
+// import json file dynamically put image in
+// remember that the image should be set as a video check mdn docs, also may be best to add the title underneath, make the likes a seperate component
 
-export default function HeroVideo() {
+export default function HeroVideo(props) {
   return (
-    <img className='img-test' src={fullScreen} alt='some filler'/>
+    <img className='hero__img' src ={props.allVideos[0].image} alt='some lads doing some bmx biking' />
   )
 }
