0c284ea607ca963b051fd87ad3d14c3bcfef45a6
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2021-10-27 10:49:57 -0400

    built the main component for the page, need to add the paragraph section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,23 @@
 import './Main.scss'
+// more than likely will need to import the images as props and pass down as before, something is rendering worry about the styling of it at a later time
 
 
 export default function Main(props) {
   return (
-    <div className='main__header' src={props.videoDetails[0].title} >Words on the screen Cant reckon what to do, I want this to be an h1</div> 
+    <>
+    <h1 className='main__header'>{props.videoDetails[0].title}</h1>
+    <div>
+      <h3 className='main__author'>{props.videoDetails[0].channel}</h3>
+      <p className='main__date'>{props.videoDetails[0].timestamp}</p>
+    </div>
+    <div>
+    <p><img src='../../assets/images/Icons/views.svg'
+    alt='this is an icon of an eye'/> {props.videoDetails[0].views}</p>
+    <p> <img src='../../assets/images/Icons/likes.svg'
+    alt='this is an icon of a heart'/>{props.videoDetails[0].likes}</p>
+    </div>
+    
+    </>
   )
 }
 
