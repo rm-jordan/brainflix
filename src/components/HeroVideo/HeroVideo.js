d779f9b1a77413ae3e5135babd87d46142dbbf9d
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2021-10-28 11:03:14 -0400

    worked on getting the comments mapped

--- src/components/HeroVideo/HeroVideo.js
+++ src/components/HeroVideo/HeroVideo.js
@@ -2,8 +2,14 @@ import './HeroVideo.scss'
 // import json file dynamically put image in
 // remember that the image should be set as a video check mdn docs, also may be best to add the title underneath, make the likes a seperate component
 
+//this is not rendering I will need some help
+
 export default function HeroVideo(props) {
   return (
-    <img className='hero__img' src ={props.allVideos[0].image} alt='some lads doing some bmx biking' />
+    <section>
+      <video className='hero__img'>
+        <source src={props.allVideos[0].image}></source>
+      </video>
+    </section>
   )
 }
--- src/components/PostedComments/PostedComments.js
+++ src/components/PostedComments/PostedComments.js
@@ -1,53 +1,31 @@
 import './PostedComments.scss'
 import defaultImg from '../../assets/images/Images/default.png'
 
-//check out your bandsite and see how you laid this part out so that you can style it properly//
 
-//can probably use same default image you used from bandsite as well// 
-
-//you got this//
 
 ///unsure why the below part will not render
 
 export default function CommentsPosted(props) {
   return (
   <section className='posted'>
-    <article className='posted__container'>
-    <img className='posted__img' src={defaultImg} alt='default avatar' />
+
+  {props.videoDetails[0].comments.map(
+    (comment) => (
+      // console.log(comment)
+  
+        <article className='posted__container'>
+    <img className='posted__img' 
+    src={defaultImg} 
+    alt='default avatar' />
         <div>
           <div>
-
-
-            <h4>{props.videoDetails[0].name}</h4>
-            <p>{props.videoDetails[1].timestamp}</p>
+            <h4>{comment.name}</h4>
+            <p>{comment.timestamp}</p>
           </div>
-          <p>{props.videoDetails[0].comment}</p>
-
+          <p>{comment.comment}</p>
         </div>
     </article>
-    <article className='posted__container'>
-    <img className='posted__img' src={defaultImg} alt='default avatar' />
-        <div>
-          <div>
-            <h4>Name</h4>
-            <p>Date</p>
-          </div>
-          <p>This is where the long paragraph goes.</p>
-        </div>
-    </article>
-    <article className='posted__container'>
-    <img className='posted__img' src={defaultImg} alt='default avatar' />
-        <div>
-          <div>
-            <h4>Name</h4>
-            <p>Date</p>
-          </div>
-          <p>This is where the long paragraph goes.</p>
-        </div>
-    </article>
-  </section>
+    )
+  )}
 
-
-  )
-
-}
+  </section> )};
