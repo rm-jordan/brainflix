4e292ba279fbb2689177efa7295afe60fe50dae6
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2021-10-29 19:48:26 -0400

    aligned videollist and styled a bit more, picture sizing will need some work, general layout nearing completion

--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -1,13 +1,6 @@
 import './VideoList.scss'
 
 
-// going to need to make this render based on the video at the top being selected, 
-
-// A TOTAL GUESS ON HOW TO DO THIS
-
-
-//ex if video.(name or id?) !== videoshown at top return (html parameters with video list inside minus the one shown on top) also need to connect the herovideo.js? create some function
-
 export default function VideoList(props){
 
   return (
@@ -18,13 +11,15 @@ export default function VideoList(props){
           return (<li key={video.id} onClick={()=> {
             props.handleVideoChange(video.id)
           }}>
+            <div className='videolist__img__container'>
             <img className='videolist__img' 
             src={video.image} alt={video.title} />
+            </div>
               <div>
-                <h3>
+                <h2>
                 {video.title}
-                </h3>
-                <h4>{video.channel}</h4>
+                </h2>
+                <h3>{video.channel}</h3>
               </div>
             </li>
           )
