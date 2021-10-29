8d5ebf7218063a6930de3fbe80286dca411484a8
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2021-10-29 17:55:21 -0400

    added demifont but need so fix source path, sorted most of the styling for the main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,14 +6,14 @@ import likes from '../../assets/images/Icons/likes.svg'
 
 export default function Main(props) {
   return (
-    <section className='main__container'>
-    <h1 className='main__header'>{props.currentVideo.title}</h1>
-    <div className='flex-test'>
-    <div className=''>
-      <h3 className='main__author'>{props.currentVideo.channel}</h3>
+    <section className='main'>
+    <h2 className='main__header'>{props.currentVideo.title}</h2>
+    <div className='main__container'>
+    <div className='main__left'>
+      <h3 className='main__author'>By {props.currentVideo.channel}</h3>
       <p className='main__date'>{new Date(props.currentVideo.timestamp).toLocaleDateString()}</p>
     </div>
-    <div>
+    <div className='main__right'>
     <p><img src={views}
     alt='this is an icon of an eye'/> {props.currentVideo.views}</p>
     <p> <img src={likes}
