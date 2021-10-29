e5d39170f572b84514fede306a0546c3e36a8284
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2021-10-29 15:51:19 -0400

    fixed font path error, started header styling for mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,10 +9,12 @@ export default function Header () {
   return (
     <header className="header">
       <div className ="header__container">
+        <div className='header__logo__container'>
         <a className="header__logo" href= "../../../public/index.html" >
         <img className="header__logo__img"  src={logo} 
         alt='this is the logo for the Brainflix website' />
         </a>
+        </div>
           <div className='header__search__container'>
             <input className="header__search" type='text' id='search' placeholder='Search'></input>
             <img className = 'header__img' src={defaultPic} alt='small icon of default a person' />
