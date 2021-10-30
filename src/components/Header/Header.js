0898a6e6b1aa88f4f8dd25cdf02da1dc09dd0afc
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2021-10-30 16:00:48 -0400

    fixed positioning and layout of header component, will continue tablet styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,44 @@
-import './Header.scss'
-import logo from '../../assets/images/Logo/BrainFlix-logo.svg'
-import defaultPic from '../../assets/images/Images/Mohan-muruge.jpg'
-import buttonIcon from '../../assets/images/Icons/upload.svg'
+import "./Header.scss";
+import logo from "../../assets/images/Logo/BrainFlix-logo.svg";
+import defaultPic from "../../assets/images/Images/Mohan-muruge.jpg";
+import buttonIcon from "../../assets/images/Icons/upload.svg";
 
-
-
-export default function Header () {
+export default function Header() {
   return (
     <header className="header">
-      <div className ="header__container">
-      <a className="header__logo" href= "../../../public/index.html" >
-      <img className="header__logo__img"  src={logo} 
-      alt='this is the logo for the Brainflix website' />
-      </a>
-        <div className='header__logo__container'>
+      <div className="header__container">
+        <a className="header__logo" href="../../../public/index.html">
+          <img
+            className="header__logo__img"
+            src={logo}
+            alt="this is the logo for the Brainflix website"
+          />
+        </a>
+        <div className="header__search__container">
+          <input
+            className="header__search"
+            type="text"
+            id="search"
+            placeholder="Search"
+          ></input>
+          <img
+            className="header__img"
+            src={defaultPic}
+            alt="small icon of default a person"
+          />
         </div>
-          <div className='header__search__container'>
-            <input className="header__search" type='text' id='search' 
-            placeholder='Search'>
-            </input>
-            <img className = 'header__img' src={defaultPic} alt='small icon of default a person' />
-          </div>
 
-      <div className="header__button__container">
-      <button className='header__button'><img className='header__button__icon' 
-      src={buttonIcon} alt='upload icon'
-      />UPLOAD</button>
-      </div>
+        <div className="header__button__container">
+          <button className="header__button">
+            <img
+              className="header__button__icon"
+              src={buttonIcon}
+              alt="upload icon"
+            />
+            UPLOAD
+          </button>
+        </div>
       </div>
     </header>
-  )
+  );
 }
