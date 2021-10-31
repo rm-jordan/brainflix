3658c25816934cfe9ceaada9f0b641920e44da64
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2021-10-31 14:13:43 -0400

    fixed all px to rem, deleted all unneeded console.logs, adjusted code layout

--- src/components/CommentBox/CommentBox.js
+++ src/components/CommentBox/CommentBox.js
@@ -1,29 +1,34 @@
-import './CommentBox.scss'
-import defaultPic from '../../assets/images/Images/Mohan-muruge.jpg'
-import plus from '../../assets/images/Icons/add_comment.svg'
+import "./CommentBox.scss";
+import defaultPic from "../../assets/images/Images/Mohan-muruge.jpg";
+import plus from "../../assets/images/Icons/add_comment.svg";
 
-//should I set this up somehow that I can make this work to add comments is this done in the same function or as set of functions?
-
-
-///still needs adjustment
-
-export default function Comments(){
+export default function Comments() {
   return (
-    <section className='commentbox'>
-    <h2 className='commentbox__header'>JOIN THE CONVERSATION</h2>
-    <div className='commentbox__section__container'>
-    <div className='commentbox__container'>
-    <img className = 'commentbox__img' src={defaultPic} alt='small icon of default a person' />
-      <form className='commentbox__form'>
-        <textarea className='commentbox__textarea' placeholder='  Add a new comment'>
-        </textarea>
-        <button className='commentbox__button'>
-        <img className='commentbox__button__icon' 
-        src={plus} alt='upload icon'
-        />COMMENT</button>
-      </form>
-      </div>
+    <section className="commentbox">
+      <h2 className="commentbox__header">JOIN THE CONVERSATION</h2>
+      <div className="commentbox__section__container">
+        <div className="commentbox__container">
+          <img
+            className="commentbox__img"
+            src={defaultPic}
+            alt="small icon of default a person"
+          />
+          <form className="commentbox__form">
+            <textarea
+              className="commentbox__textarea"
+              placeholder="  Add a new comment"
+            ></textarea>
+            <button className="commentbox__button">
+              <img
+                className="commentbox__button__icon"
+                src={plus}
+                alt="upload icon"
+              />
+              COMMENT
+            </button>
+          </form>
+        </div>
       </div>
     </section>
-  )
+  );
 }
--- src/components/PostedComments/PostedComments.js
+++ src/components/PostedComments/PostedComments.js
@@ -1,31 +1,21 @@
-import './PostedComments.scss'
-import defaultImg from '../../assets/images/Images/default.png'
-
-//needs lots of styling work, will have to reference setup from bandsite for similar characteristics of html alignments
-
-
-//may need to change some of these element titles,
-// will adjust as required
+import "./PostedComments.scss";
+import defaultImg from "../../assets/images/Images/default.png";
 
 export default function CommentsPosted(props) {
   return (
-  <section className='posted'>
-
-  {props.videoDetails[0].comments.map(
-    (comment) => (
-        <article className='posted__container'>
-        <img className='posted__img' 
-        src={defaultImg} 
-        alt='default avatar' />
-        <div className='posted__comment'>
-          <div  className='posted__info'>
-            <h4>{comment.name}</h4>
-            <p>{new Date(comment.timestamp).toLocaleDateString()}</p>
+    <section className="posted">
+      {props.videoDetails[0].comments.map((comment) => (
+        <article className="posted__container">
+          <img className="posted__img" src={defaultImg} alt="default avatar" />
+          <div className="posted__comment">
+            <div className="posted__info">
+              <h4>{comment.name}</h4>
+              <p>{new Date(comment.timestamp).toLocaleDateString()}</p>
+            </div>
+            <p>{comment.comment}</p>
           </div>
-          <p>{comment.comment}</p>
-        </div>
-    </article>
-    )
-  )}
-
-  </section> )};
+        </article>
+      ))}
+    </section>
+  );
+}
