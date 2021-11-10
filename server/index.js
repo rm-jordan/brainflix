706f5509f10cd1eed17d2983d94b5c3cbb38fe29
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2021-11-10 10:36:18 -0500

    testing node mods

---
+++ server/index.js
@@ -0,0 +1,25 @@
+//index server
+const express = require("express");
+
+// const routeName = require('./routes/routeName');
+
+const app = express();
+
+// API INFO FROM SPRINT -2
+// `https://project-2-api.herokuapp.com/videos?api_key=47ffd825-f3d6-483b-ae09-531887cd1206`
+
+// going to need to establish a few routes
+
+// use the port 3000? <- the local host?
+
+//access the public folder which is static and get the photos
+
+// json call
+
+//app.use() <-- one for each of the routes
+
+//app.get('/', (req, res) => {
+//  res.send('is this working?')
+// })
+
+// then  I need to listen for it on the port
---
+++ server/routes/videos.js
@@ -0,0 +1,36 @@
+// this is where I will need to route the files
+
+const express = require("express");
+const router = express.Router();
+
+// const router = express.router()
+
+// I need to get the videos into the file using JSON
+
+// get an array from the videos in the data
+
+// set up express router
+//example from class
+// const saladArray = ["Caesar", "Greek", "Garden"];
+// const soupArray = ["Vegetable", "Chicken", "Tomato"];
+// router.get("/salad", (req, res) => {
+//   res.json({
+//     salads: saladArray,
+//   });
+// });
+
+// router.get("/soup", (req, res) => {
+//   res.json({
+//     soups: soupArray,
+//   });
+// });
+
+// module.exports = router;
+
+// establish at least two routes, do they need to be seperate files?
+
+//router.get('/' (req,res) => {
+//   __________________________
+// })
+
+// module.exports =router;
